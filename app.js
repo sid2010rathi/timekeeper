c9f421a3e5075f088ea447edaa83d336ca25e8b0
Siddharth Rathi <69364115+sid2010rathi@users.noreply.github.com>
2021-02-17 16:38:38 -0500

    setup heroku server

---
+++ app.js
@@ -0,0 +1,32 @@
+const cors = require('cors');
+var createError = require('http-errors');
+const bodyParser = require('body-parser')
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var app = express();
+app.use(cors());
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+//Path of react build
+//app.use(express.static(path.join(__dirname,'', '')));
+
+//add routes here
+
+app.get("/", sayhi);
+
+function sayhi(req, res) {
+    res.json("Success!! Server is running.");
+    //SRN@1234:finalprojectmean@gmail.com
+}
+
+app.listen(3000, () => {
+    console.log("Running on 3000 port");
+})
