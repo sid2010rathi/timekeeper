25a20149d1da7f932346cb0250c8a0c25521ec90
Siddharth <69364115+sid2010rathi@users.noreply.github.com>
2021-04-14 14:06:55 -0400

    Fix API

--- routes/login.js
+++ routes/login.js
@@ -2,7 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 const loginCtrl = require('../controller/login');
-
-router.post('/', loginCtrl.login);
+const methodNotAllowed = (req, res, next) => res.status(405).send();
+router
+.route('/')
+.post(loginCtrl.login)
+.all(methodNotAllowed);
+//router.post('/', loginCtrl.login);
 
 module.exports = router;
